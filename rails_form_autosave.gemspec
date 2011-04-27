d9a73f6ce7f2321d39a267cc100703273dd40920
David Ruyer <druyer@hypios.com>
2011-04-27 18:55:08 +0200

    First commit

---
+++ rails_form_autosave.gemspec
@@ -0,0 +1,9 @@
+# Provide a simple gemspec so you can easily use your enginex
+# project in your rails apps through git.
+Gem::Specification.new do |s|
+  s.name = "rails_form_autosave"
+  s.summary = "Insert RailsFormAutosave summary."
+  s.description = "Insert RailsFormAutosave description."
+  s.files = Dir["{app,lib,config}/**/*"] + ["MIT-LICENSE", "Rakefile", "Gemfile", "README.rdoc"]
+  s.version = "0.0.1"
+end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'cdf7ee3f5b0a163a23f656e9d159e4945e31a1525d698d9d38c45e51017fb41eda1d3dcac3449bcfceec8082faefa28a5caa6cb26f273f4e6c732227fb0f9040'
