d9d99bfef61e6ff2607b39e86658d0c5e34fce9b
David Ruyer <druyer@hypios.com>
2011-04-29 01:38:18 +0200

    JS done New action to clear the form

--- test/dummy/public/javascripts/rails-form-autosave.js
+++ test/dummy/public/javascripts/rails-form-autosave.js
@@ -1,17 +1,56 @@
 (function($) {
+
+  $.fn.formId = function() {
+    return $(this).attr('autosave_id');
+  }
+   
+  $.fn.loadFields = function() {
+    form = $(this)
+    
+    // Update the values
+    $.getJSON('/rails_form_autosave/load/'+$(this).formId(), function(data){
+      $.each(data,function(id, value){
+        form.find('#'+id).val(value);
+      });
+    });   
+  }
+
+  $.fn.saveFields = function() {
+    form = $(this)
+    
+    // Get the fields values
+    var params = {};
+    form.find('input:visible').each(function(){
+      var input = $(this)
+      if (input.attr('id').length > 0) {
+        params[input.attr('id')] = input.val();
+      }
+    });
+    
+    // Send them to be savec
+    $.post('/rails_form_autosave/save/'+form.formId(), $.param({rails_form_autosave:params}));
+  }
+   
+  $.fn.clearFields = function() {
+    $.get('/rails_form_autosave/clear/'+$(this).formId());
+  } 
    
   $.fn.autosaveFields = function() {
     var form = $(this);
-    var form_id = $(this).attr('autosave_id');
     
+    // Load the fields
+    form.loadFields();
+
+    // Save the fields when edited
+    form.find('input:visible').each(function(){
+      input = $(this);
+      input.blur(function(){form.saveFields();});
+    });
     
-    
-    // Step 1 : load the autosaved data
-    
-    // Step 2 : once it's done, monitor the fields and periodically send updates
-    
-    // Bonuses : clear saved fields when form is sent
-    
+    // Clear the fields when the form is submitted
+    form.submit(function(){
+      form.clearFields();
+    });
   }
 
   // Main hook
